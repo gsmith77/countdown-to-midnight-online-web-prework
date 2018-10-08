92a5eb8cb3a83301061f7f39cc2f2dc232e870dd
grant smith <grantsmith1256@gmail.com>
2018-10-08 21:38:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 def countdown(number)
   number=10
   while number>0
-  puts "#{number} SECOND(S)!"
+  return "#{number} SECOND(S)!"
   number-=1
 end
 end
 
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
