1002ff5f4d8f7797b2991cd1e4d0fc7a089178f3
grant smith <grantsmith1256@gmail.com>
2018-10-08 22:08:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  number=10
+  while number>0
+  puts "#{number} SECOND(S)!"
+  number-=1
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x) 
+  x=5
+  while x>0
+  puts "#{x} SECOND(S)!"
+  x-=1
+  sleep 1
+end
+end
+  
